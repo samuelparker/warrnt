a8308ebc6704d7a5db9d8f9cd87e7c75061dd512
Sam Parker <samuel.gage.parker@gmail.com>
2016-09-17 13:32:23 -0700

    adjust format of seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,66 +13,7 @@ end
   Contact.create(name: Faker::Name.name, email: Faker::Internet.email)
 end
 
-# General Resources
-Resource.create(description: "", url: "" )
-Resource.create(description: "", url: "" )
-Resource.create(description: "", url: "" )
-Resource.create(description: "", url: "" )
-Resource.create(description: "", url: "" )
-
-# Rudeness Resources
-Resource.create(description: "", url: "" )
-Resource.create(description: "", url: "" )
-
-# Excessive Force
-Resource.create(description: "", url: "" )
-Resource.create(description: "", url: "" )
-
-# Bribes
-Resource.create(description: "", url: "" )
-Resource.create(description: "", url: "" )
-
-# Drinking
-Resource.create(description: "", url: "" )
-Resource.create(description: "", url: "" )
-
-# Harassment
-Resource.create(description: "", url: "" )
-Resource.create(description: "", url: "" )
-
-# False Report
-Resource.create(description: "", url: "" )
-Resource.create(description: "", url: "" )
-
-# Narcotic Use
-Resource.create(description: "", url: "" )
-Resource.create(description: "", url: "" )
-
-# Discrimination
-Resource.create(description: "", url: "" )
-Resource.create(description: "", url: "" )
-
-# Alter Official Docs
-Resource.create(description: "", url: "" )
-Resource.create(description: "", url: "" )
-
-# Careless Driving
-Resource.create(description: "", url: "" )
-Resource.create(description: "", url: "" )
-
-# Racial Intimidation
-Resource.create(description: "", url: "" )
-Resource.create(description: "", url: "" )
-
-# Threats or Assault
-Resource.create(description: "", url: "" )
-Resource.create(description: "", url: "" )
-
-# Sexual Harassment
-Resource.create(description: "", url: "" )
-Resource.create(description: "", url: "" )
-
-Tag.create(name: "Rudeness")
+Tag.create(name: "General")
 Tag.create(name: "Excessive Force")
 Tag.create(name: "Soliciting or Accepting Bribes")
 Tag.create(name: "Drinking on Duty")
@@ -85,3 +26,116 @@ Tag.create(name: "Careless Driving")
 Tag.create(name: "Racial or Ethnic Intimidation")
 Tag.create(name: "Malicious Threats or Assault")
 Tag.create(name: "Sexual Harassment")
+
+# General Resources
+Resource.create(description: "City of San Francisco Office of Citizen Complaints",
+                        url: "http://sfgov.org/occ/"
+                ).tags << Tag.find_by(name: "General")
+Resource.create(description: "ACLU of Northern California",
+                        url: "https://www.aclunc.org/"
+                ).tags << Tag.find_by(name: "General")
+Resource.create(description: "SFPD Rules of Conduct",
+                        url: "http://sanfranciscopolice.org/sites/default/files/FileCenter/Documents/14819-DGO2.01.pdf"
+                ).tags << Tag.find_by(name: "General")
+Resource.create(description: "SFPD Citizen Complaint Policy",
+                        url: "http://sanfranciscopolice.org/sites/default/files/FileCenter/Documents/14816-DGO2.04.pdf"
+                ).tags << Tag.find_by(name: "General")
+Resource.create(description: "How to File a Complaint with The US DOJ",
+                        url: "https://www.justice.gov/crt/how-file-complaint"
+                ).tags << Tag.find_by(name: "General")
+
+# Excessive Force
+Resource.create(description: "SFPD Use of Force Policy",
+                        url: "http://sanfranciscopolice.org/sites/default/files/Documents/PoliceCommission/PoliceCommission040616-040616stakeholdersletters.pdf"
+                        ).tags << Tag.find_by(name: "Excessive Force")
+Resource.create(description: "",
+                        url: ""
+                        ).tags << Tag.find_by(name: "Excessive Force")
+
+# Bribes
+Resource.create(description: "",
+                        url: ""
+                ).tags << Tag.find_by(name: "Soliciting or Accepting Bribes")
+Resource.create(description: "",
+                        url: ""
+                ).tags << Tag.find_by(name: "Soliciting or Accepting Bribes")
+
+# Drinking
+Resource.create(description: "",
+                        url: ""
+                ).tags << Tag.find_by(name: "Drinking on Duty")
+Resource.create(description: "",
+                        url: ""
+                ).tags << Tag.find_by(name: "Drinking on Duty")
+
+# Harassment
+Resource.create(description: "",
+                        url: ""
+                ).tags << Tag.find_by(name: "Harassment")
+Resource.create(description: "",
+                        url: ""
+                ).tags << Tag.find_by(name: "Harassment")
+
+# False Report
+Resource.create(description: "",
+                        url: ""
+                ).tags << Tag.find_by(name: "Making a False Report")
+Resource.create(description: "",
+                        url: ""
+                ).tags << Tag.find_by(name: "Making a False Report")
+
+# Narcotic Use
+Resource.create(description: "",
+                        url: ""
+                ).tags << Tag.find_by(name: "Use of Narcotics (On or Off Duty)")
+Resource.create(description: "",
+                        url: ""
+                ).tags << Tag.find_by(name: "Use of Narcotics (On or Off Duty)")
+
+# Discrimination
+Resource.create(description: "",
+                        url: ""
+                ).tags << Tag.find_by(name: "Discrimination")
+Resource.create(description: "",
+                        url: ""
+                ).tags << Tag.find_by(name: "Discrimination")
+
+# Alter Official Docs
+Resource.create(description: "",
+                        url: ""
+                ).tags << Tag.find_by(name: "Altering Information on an Official Document")
+Resource.create(description: "",
+                        url: ""
+                ).tags << Tag.find_by(name: "Altering Information on an Official Document")
+
+# Careless Driving
+Resource.create(description: "",
+                        url: ""
+                ).tags << Tag.find_by(name: "Careless Driving")
+Resource.create(description: "",
+                        url: ""
+                ).tags << Tag.find_by(name: "Careless Driving")
+
+# Racial Intimidation
+Resource.create(description: "",
+                        url: ""
+                ).tags << Tag.find_by(name: "Racial or Ethnic Intimidation")
+Resource.create(description: "",
+                        url: ""
+                ).tags << Tag.find_by(name: "Racial or Ethnic Intimidation")
+
+# Threats or Assault
+Resource.create(description: "",
+                        url: ""
+                ).tags << Tag.find_by(name: "Malicious Threats or Assault")
+Resource.create(description: "",
+                        url: ""
+                ).tags << Tag.find_by(name: "Malicious Threats or Assault")
+
+# Sexual Harassment
+Resource.create(description: "",
+                        url: ""
+                ).tags << Tag.find_by(name: "Sexual Harassment")
+Resource.create(description: "",
+                        url: ""
+                ).tags << Tag.find_by(name: "Sexual Harassment")
